00da157a4a5d0063cf0865659b71c1d1650a16a6
Mohammad Hosein Fallah <emhashef@gmail.com>
2020-12-27 13:40:02 +0330

    add email verify middlewares

--- routes/property.php
+++ routes/property.php
@@ -5,11 +5,15 @@ use App\Http\Controllers\PropertyController;
 use Illuminate\Support\Facades\Route;
 
 
-Route::get('property/{property}/accept',[PropertyController::class,'accept'])->name('property.accept');// defined GET for easy impelemntation
+Route::get('property/{property}/accept',[PropertyController::class,'accept'])
+            ->name('property.accept')
+            ->middleware('auth')// defined GET for easy impelemntation
+            ->middleware('verified');// defined GET for easy impelemntation
 
 Route::resource('property',PropertyController::class)->only([
     'show','create','store','index'
-]);
+])->middleware('auth')
+->middleware('verified');
 
 
 
